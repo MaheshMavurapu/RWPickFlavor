5d739af2ba80d977520f3dc528895d8af508f319
MaheshMavurapu <mahesh.mavurapu@gmail.com>
2018-12-18 23:26:05 +0530

    Initial Commit

---
+++ RWPickFlavor/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  RWPickFlavor
+//
+//  Created by Mahesh Mavurapu on 18/12/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() { }
+    
+    public static func getSomething() -> String {
+        return "Mahesh Mavurapu"
+    }
+}
